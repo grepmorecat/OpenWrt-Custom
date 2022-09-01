e6d6f3a6a8355b55e531324e3113405415c88599
Jin Yijie <jinyijie@outlook.com>
2022-09-01 22:48:13 +0800

    Update device-env.sh

--- device-env.sh
+++ device-env.sh
@@ -50,8 +50,8 @@ case $1 in
     6)
         DEVICE_TAG="OpenWrt stable RE-SP-01B"
         REPO_USE=openwrt/openwrt
-        REPO_BRANCH=v21.02.3
-        COMMIT_SHA=latest
+        REPO_BRANCH=openwrt-21.02
+        COMMIT_SHA=42a15ca
         DIY_P2_SH="diy-part2/[OpenWrt]RE-SP-01B-part2.sh"
         DEPENDS=$(curl -fsSL "https://gist.githubusercontent.com/1-1-2/38e424cd9da729f72fa4a495d23271ea/raw/openwrt")
         SEQ_FILE="testSeq/openwrt.ini"
